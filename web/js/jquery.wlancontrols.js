3c3d2d96f09070081461be1c6ea64a1a0b476877
Benoit Viguier <benoit.viguier@elao.com>
2015-08-14 22:37:15 +0200

    Improved wlancontroler to limit dependencies

--- web/js/jquery.wlancontrols.js
+++ web/js/jquery.wlancontrols.js
@@ -1,18 +1,64 @@
-$.fn.wlancontrols = function(){
-    $(this).append('<div id="wlancontrols__container">'+
+$.fn.wlancontrols = function(options){
+
+    var opts      = $.extend( {}, $.fn.wlancontrols.defaults, options );
+    var container = $(this);
+
+    container.html(
         '<div class="info">'+
         '<form class="form-inline">'+
             '<div class="form-group">'+
                 '<label for="server">Server</label> '+
-                '<input type="text" value="localhost" name="server" id="server" /> '+
+                '<input type="text" value="localhost" name="server" id="wlancontrols-server" /> '+
                 '<label for="port">Port</label> '+
-                '<input type="text" name="port" value="8080" id="port" /> '+
-                '<button type="button" class="btn btn-success"   id="connect"    >Connect</button> '+
-                '<button type="button" class="btn btn-danger"    id="disconnect" >Disconnect</button>'+
+                '<input type="text" name="port" value="8080" id="wlancontrols-port" /> '+
+                '<button type="button" class="btn btn-success"   id="wlancontrols-connect"    >Connect</button> '+
+                '<button type="button" class="btn btn-danger"    id="wlancontrols-disconnect" disabled="disabled">Disconnect</button>'+
             '</div>'+
         '</form>'+
         '</div>'+
-        '<div class="controlbar"><i class="glyphicon glyphicon-th-list" id="wlancontrols-opener"></i></div>'+
-    '</div>');
+        '<div class="controlbar"><i class="glyphicon glyphicon-th-list" id="wlancontrols-opener"></i></div>'
+    );
+    container.toggleClass('wlancontrols__container', true);
+    container.toggleClass('open', true);
+
+    this.connectButton      = $('#wlancontrols-connect', container);
+    this.disconnectButton   = $('#wlancontrols-disconnect', container);
+
+    var serverTxt   = $('#wlancontrols-server', container),
+        portTxt     = $('#wlancontrols-port', container);
+    this.serverVal  = function() {return serverTxt.val();};
+    this.portVal    = function() {return portTxt.val();};
+
+    //Opener callback
+    $('#wlancontrols-opener', container).click(function(){
+        container.toggleClass('open');
+    });
+
+    //On connect
+    this.connectButton.click(function(){
+        opts.onConnect();
+        container.toggleClass('open', false);
+
+        serverTxt.prop('disabled', true);
+        portTxt.prop('disabled', true);
+        $('.btn', container).prop('disabled', false);
+        $(this).prop('disabled', true);
+    });
+
+    //On disconnect
+    this.disconnectButton.click(function() {
+        opts.onDisconnect();
+
+        serverTxt.prop('disabled', false);
+        portTxt.prop('disabled', false);
+        $('button', container).prop('disabled', false);
+        $(this).prop('disabled', true);
+    });
+
     return this;
+};
+
+$.fn.wlancontrols.defaults = {
+    onConnect : function() {},
+    onDisconnect : function() {}
 };
